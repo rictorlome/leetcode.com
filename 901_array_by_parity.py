770c0cc13f76b9054c19a1ea1b01d94adb9ae9e4
rictorlome <rictorlome@gmail.com>
2019-05-08 20:43:15 -0400

    Sort array by parity

---
+++ 901_array_by_parity.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        evens, odds = [], []
+        for val in A:
+            if val % 2 == 0:
+                evens.append(val)
+            else:
+                odds.append(val)
+        return evens + odds
